91241c1206da104843ce0c079676dda76476ba1a
Synth <ecolatez@gmail.com>
2025-02-21 12:31:58 +0300

    Practice

---
+++ Huy.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(void)
+{
+	printf("Пизде, везде разные шорткаты бля я ебу\n");
+	return 0;
+}
+
+it main_2(void)
+{
+	printf("НИхуя не понимаю");
+	return 0;
+}
+
+
---
+++ practice.py
@@ -0,0 +1,8 @@
+
+for _ in range(100):
+    print('zalupa')
+    for _ in range(100):
+        print("paizda")
+        for _ in range(100):
+            print('huy')
+    
