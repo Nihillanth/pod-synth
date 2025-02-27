15fa0021dea68d7492dec340e4cf2520649562e3
Synth <ecolatez@gmail.com>
2025-02-27 11:15:18 +0300

    Переименовал файлы. 	new file:   practice_.c 	new file:   practice_.exe 	new file:   practice_.py

---
+++ practice_.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main(void)
+{
+	printf("Esli t ieto chitaesh to u tebya vse poluchilos")
+	printf("Pizdec, yebal ya etot C\n");
+	printf("Privet, Pidori i sasite jopu\n");
+	return 0;
+}
+
